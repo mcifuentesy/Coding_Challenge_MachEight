120746429fedb7885293cb315bc30fda22318bf4
mcifuentesy <71228091+mcifuentesy@users.noreply.github.com>
2022-12-07 14:25:47 -0500

    Update find_pairs_from_sum.py

--- find_pairs_from_sum.py
+++ find_pairs_from_sum.py
@@ -7,8 +7,8 @@ def find_pairs_integers_sum(arr, n, sum): #Time complexity is O(n) / Linear Time
             loop_numbers_collection[arr[i]] = 1 #loop_numbers_collection (save numbers into a dictionary)
 
 if __name__ == '__main__':
-    numbers_to_evaluate = str(input('Enter only integer numbers separated by space (no repeated) to find pairs of numbers that result in a definied sum: ')) # 1 9 5 0 20 -4 12 16 7
-    target_sum = int(input('Enter the target sum (only an integer): '))# 12
+    numbers_to_evaluate = str(input('Enter only integer numbers separated by space (no repeated) to find pairs of numbers that result in a definied sum: ')) #Example: 1 9 5 0 20 -4 12 16 7
+    target_sum = int(input('Enter the target sum (only an integer): '))#Example 12
     user_list = numbers_to_evaluate.split()
     user_list = [int(i) for i in user_list]
     total_numbers = len(user_list)
